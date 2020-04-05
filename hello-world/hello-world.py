f6040477308e2b999c514238873e6269a51f4c8b
Gaurav Madaan <gaurav.madaan@technossus.com>
2020-04-05 11:41:30 +0530

    Added hello World

---
+++ hello-world/hello-world.py
@@ -0,0 +1,14 @@
+# This is how you print the statement
+print('Hello World')
+
+"""
+This program is teaching you:
+1. How to add comments?
+2. How to print statement?
+3. How to declare variables?
+"""
+
+a = input("Enter first number ")
+b = input("Enter second number ")
+c = a + b
+print(c)
